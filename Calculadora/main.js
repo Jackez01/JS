0e4e08b851fb922fd47a68087993a0a46bcb9436
Lucas Geraldi Freitas <lucasgefreitas@gmail.com>
2026-01-22 21:42:14 -0300

    Lógica calculadora

--- Calculadora/main.js
+++ Calculadora/main.js
@@ -1,13 +1,21 @@
 const botoes = document.querySelector("#btns");
 const scream = document.querySelector("#display");
 
+let primeiroNumero;
+let operador;
+
 botoes.addEventListener("click", (event) => {
     event.preventDefault();
+    
 
+    if(!isNaN( event.target.innerText)) {
 
-    if()
     //Soma o valor atual do display mais o novo botão selecionado
-     scream.value = (scream.value) + (event.target.innerText);
-      
-
-})
+    scream.value = (scream.value) + (event.target.innerText);
+    } if (isNaN(event.target.innerText)) {
+        primeiroNumero = scream.value;
+        operador = event.target.innerText;
+        scream.value = "";
+    }
+    
+    });
